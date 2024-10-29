9baca611f2bd5a9d3fc27c0be3de005ababcbb54
Arvind M Bharadwaj <arvi2014.m@gmail.com>
2024-10-30 01:19:42 +0530

    Fixed Generator modal opening

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -178,41 +178,7 @@ const Navbar = () => {
         message="Are you sure you want to log out?"
         confirmText="Yes, I'm sure"
       />
-
-      {isGeneratorModalOpen && (
-        <div
-          id="generatorModal"
-          tabIndex="-1"
-          aria-hidden="true"
-          className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-full bg-black bg-opacity-50"
-        >
-          <div className="relative p-6 w-full max-w-2xl h-fit mx-auto">
-            <div className="relative bg-white rounded-lg shadow-lg dark:bg-gray-800 w-full h-full p-8">
-              <button
-                type="button"
-                className="text-gray-400 absolute top-3 right-3 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-2"
-                onClick={() => setIsGeneratorModalOpen(false)}
-              >
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-              <Generator setIsGeneratorModalOpen={setIsGeneratorModalOpen} />
-            </div>
-          </div>
-        </div>
-      )}
+      <Generator isOpen={isGeneratorModalOpen} setIsGeneratorModalOpen={setIsGeneratorModalOpen} />
     </>
   );
 };
